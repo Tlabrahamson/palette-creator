796734171438973fd4ac1c8d71535343b7dc6abf
Tim Abrahamson <tim@kinetic.com>
2020-05-24 15:48:19 -0500

    Started adding functionality

--- app.js
+++ app.js
@@ -3,16 +3,69 @@ const colorDivs = document.querySelectorAll(".color");
 const generateButton = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelector(".color h2");
+let initialColors;
 
 //Functions
+
+//Random Hex Generator Function
 const generateHex = () => {
-  const letters = "0123456789ABCDEF";
-  let hash = "#";
-  for (let i = 0; i < 6; i++) {
-    hash += letters[Math.floor(Math.random() * 16)];
-  }
-  return hash;
+  const hexColor = chroma.random();
+  return hexColor;
 };
 
-let randomHex = generateHex();
-console.log(randomHex);
+const randomColors = () => {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    //Add color to the background
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+
+    //Check for contrast
+    checkTextContrast(randomColor, hexText);
+
+    //Initialize color sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
+  });
+};
+
+//Check the text contrast against the background
+const checkTextContrast = (color, text) => {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "#000";
+  } else {
+    text.style.color = "#fff";
+  }
+};
+
+const colorizeSliders = (color, hue, brightness, saturation) => {
+  //Scale saturation
+  const noSat = color.set("hsl.s", 0);
+  const fullSat = color.set("hsl.s", 1);
+  const scaleSaturation = chroma.scale([noSat, color, fullSat]);
+
+  //Scale Brightness
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBrightness = chroma.scale(["#000", midBright, "#fff"]);
+
+  //Update input colors
+  brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBrightness(
+    0
+  )}, ${scaleBrightness(0.5)}, ${scaleBrightness(1)})`;
+
+  saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSaturation(
+    0
+  )}, ${scaleSaturation(1)})`;
+
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204, 75, 75), rgb(204, 204, 75), rgb(75, 204, 75), rgb(75, 204, 204), rgb(75, 75, 204), rgb(204, 75, 204), rgb(204, 75, 75))`;
+};
+
+randomColors();
