c921c6b6a04d2f60e3828bc31c570e9722f4bfcc
Tim Abrahamson <tim@kinetic.com>
2020-05-24 15:21:55 -0500

    Styled up most of the project. Started JS

---
+++ app.js
@@ -0,0 +1,18 @@
+//Global Selections
+const colorDivs = document.querySelectorAll(".color");
+const generateButton = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelector(".color h2");
+
+//Functions
+const generateHex = () => {
+  const letters = "0123456789ABCDEF";
+  let hash = "#";
+  for (let i = 0; i < 6; i++) {
+    hash += letters[Math.floor(Math.random() * 16)];
+  }
+  return hash;
+};
+
+let randomHex = generateHex();
+console.log(randomHex);
